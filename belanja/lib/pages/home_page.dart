9247208c412781efb4698f5a026146d9760641c2
Adrifa <adrifavero24@gmail.com>
2022-04-01 16:48:21 +0700

    add Item

--- belanja/lib/pages/home_page.dart
+++ belanja/lib/pages/home_page.dart
@@ -6,15 +6,15 @@ class HomePage extends StatelessWidget {
   @override
   final List<Item> items = [
     Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000)
+    Item(name: 'Salt', price: 2000),
+    Item(name: 'Chili', price: 4000),
+    Item(name: 'Cheese', price: 100000)
   ];
 
   Widget build(BuildContext context) {
-    //TODO: implement build
-   // throw UnimplementedError();
     return Scaffold(
         appBar: AppBar(
-          title: Text("Shopping List"),
+          title: Text("Barang"),
         ),
         body: Container(
             margin: EdgeInsets.all(8),
@@ -23,7 +23,10 @@ class HomePage extends StatelessWidget {
                 itemCount: items.length,
                 itemBuilder: (context, index) {
                   final item = items[index];
-                  return InkWell(
+                    return InkWell(
+                    onTap: (){
+                      Navigator.pushNamed(context, '/item', arguments: item);
+                    },
                     child: Card(
                         child: Container(
                             margin: EdgeInsets.all(8),
@@ -38,22 +41,6 @@ class HomePage extends StatelessWidget {
                               ],
                             ))),
                   );
-                  return InkWell(
-                  onTap: (){
-                    Navigator.pushNamed(context, '/item', arguments: item);
-                  },
-                );
-
-                }))
- 
-               
-                
-                );
-
-
+                })));
   }
-
-
-
-
 }
